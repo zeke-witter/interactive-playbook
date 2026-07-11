358da7ef09d7770cc29411866efdca5ef149c882
Zekey <thevoiceofzeke@gmail.com>
2026-07-10 21:15:17 -0500

    feat: add branch types for the play designer

--- src/types/designer.ts
+++ src/types/designer.ts
@@ -1,9 +1,17 @@
 import type { PlayerState, PlayerPath, ThrowArc } from './play'
 
+export type DesignerBranch = {
+  label: string
+  steps: DesignerStep[]
+}
+
 export type DesignerStep = {
   players: PlayerState[]
   pathPreviews: PlayerPath[]
   throw?: ThrowArc
+  branches?: DesignerBranch[]
 }
 
 export type DesignerMode = 'position' | 'path' | 'throw'
+
+export type StepPath = number[]
